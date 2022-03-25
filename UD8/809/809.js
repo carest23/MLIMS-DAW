c812072bf5111376989033babe26eeef468c9cc9
carest23 <98306857+carest23@users.noreply.github.com>
2022-03-25 09:06:18 +0100

    Gallery

---
+++ UD8/809/809.js
@@ -0,0 +1,8 @@
+"use strict"
+
+function showGallery(divGallery)    {
+    let h1 = document.createElement('h1');
+    divGallery.append(h1);
+}
+
+//Aquesta setmana no li he pogut dedicar el temps suficient a fer les activitats Vicent.
---
+++ UD8/809/img/Untitled-1.js
@@ -0,0 +1,33 @@
+let harryPotter = {
+    title: 'Harry Potter 1',
+    author: 'JK Rowling',
+    publishedIn: 1997,
+    alreadyRead: true,
+    cover: {h:200, w:100, x:0, y:0, src: "/img/hp1.jpg"}
+};
+let harryPotter2 = {
+    title: 'Harry Potter 2',
+    author: 'JK Rowling',
+    publishedIn: 1998,
+    alreadyRead: false,
+    cover: {h:200, w:100, x:0, y:0, src: "/img/hp2.jpg"}
+};
+let harryPotter3 = {
+    title: 'Harry Potter 3',
+    author: 'JK Rowling',
+    publishedIn: 1999,
+    alreadyRead: false,
+    cover: {h:200, w:100, x:0, y:0, src: "/img/hp3.jpg"}
+};
+
+let harryPotter4 = {
+    title: 'Harry Potter 4',
+    author: 'JK Rowling',
+    publishedIn: 2001,
+    alreadyRead: false,
+    cover: {h:200, w:100, x:0, y:0, src: "/img/hp4.jpg"}
+};
+
+
+
+let gallery = [harryPotter, harryPotter2, harryPotter3, harryPotter4];
